5e934f50f4faa1db2d179b959be4dc0c32184796
Jintana C <jc2747@cornell.edu>
2017-12-24 13:13:02 -0500

    set repo, css changes

---
+++ main.js
@@ -0,0 +1,12 @@
+links = document.querySelectorAll(".ga-link");
+links.forEach(function (element) {
+  element.addEventListener("click", function() {
+    var name = element.getAttribute("ga-name");
+    ga('send','event','Outbound Link','click', name);
+  })
+});
+
+
+
+
+// typing effect
